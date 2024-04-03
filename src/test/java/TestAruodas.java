617750bf16726349446a9d6af031366595c877bb
Kaspis <kasparas.ziuraitis@gmail.com>
2024-04-03 13:41:51 +0300

    Aruodas-testing search

--- src/test/java/TestAruodas.java
+++ src/test/java/TestAruodas.java
@@ -19,7 +19,6 @@ public class TestAruodas {
 
     @AfterTest
     public void setUp2(){
-    _globalDriver= new ChromeDriver();
     _globalDriver.close();
     }
 
@@ -32,35 +31,58 @@ public class TestAruodas {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        WebElement cookieButton=_globalDriver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[1]/div/div[2]/div/button[1]"));
+        cookieButton.click();
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         WebElement flats = _globalDriver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[1]/div/ul[1]/li[1]/ul/li[1]"));
         flats.click();
-        WebElement forBuying= _globalDriver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[1]/div/ul[2]/li[1]"));
-        forBuying.click();
-        WebElement district =_globalDriver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[1]/div/ul[3]/li[1]/a[1]"));
+        WebElement forBuy= _globalDriver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[1]/div/ul[2]/li[1]"));
+        forBuy.click();
+        WebElement city = _globalDriver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[1]/div/ul[3]/li[1]/a[1]"));
+        city.click();// ????
+        WebElement choosingCity= _globalDriver.findElement(By.xpath("/html/body/div/ul[1]/li[2]/a"));
+        choosingCity.click();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement district = _globalDriver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[1]/div/ul[3]/li[2]/a"));
         district.click();
+        WebElement chooseDistrict= _globalDriver.findElement(By.xpath("/html/body/div/ul/li[2]/label/span[1]"));
+        chooseDistrict.click();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("submitSelect1")).click(); // issaugoti pasirinkima
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement rooms= _globalDriver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[1]/div/ul[3]/li[4]/a"));
+        rooms.click();
+        WebElement chooseRoomsMin= _globalDriver.findElement(By.xpath("/html/body/div/ul[1]/li[2]/label/span"));
+        chooseRoomsMin.click();
+        WebElement chooseRoomsMax= _globalDriver.findElement(By.xpath("/html/body/div/ul[2]/li[2]/label/span"));
+        chooseRoomsMax.click();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        WebElement search= _globalDriver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[1]/div/ul[3]/li[10]/a[2]"));
+        search.click();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
 
